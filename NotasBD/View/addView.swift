e4dd144d3f3a6607d9f5aa540d0366b1e33dd46b
Sebastian Rozo <camynator@yahoo.es>
2021-09-12 22:40:44 -0500

    Se ajusta logica para boton al momento de guardar

--- NotasBD/View/addView.swift
+++ NotasBD/View/addView.swift
@@ -24,6 +24,12 @@ class addView: UIViewController {
         titulo.text = notas?.titulo
         nota.text = notas?.nota
         fecha.date = notas?.fecha ?? Date()
+        if notas == nil {
+            validarText()
+        } else {
+            boton.backgroundColor = .systemTeal
+            validarText2()
+        }
     }
     
     //MARK: - references actions
@@ -36,5 +42,23 @@ class addView: UIViewController {
             navigationController?.popViewController(animated: true)
         }
     }
-
+    
+    func validarText() {
+        boton.isEnabled = false
+        boton.backgroundColor = .systemGray2
+        titulo.addTarget(self, action: #selector(validarTextField), for: .editingChanged)
+    }
+    func validarText2() {
+        titulo.addTarget(self, action: #selector(validarTextField), for: .editingChanged)
+    }
+    
+    @objc func validarTextField(sender: UITextField) {
+        guard let titulo2 = titulo.text, !titulo2.isEmpty else {
+            boton.isEnabled = false
+            boton.backgroundColor = .systemGray2
+            return
+        }
+        boton.isEnabled = true
+        boton.backgroundColor = .systemTeal
+    }
 }
